1b4e2ea9a9f2bee4d3f946e74646642ff24bc5b1
lixianghong <lixianghong@meshop.net>
2022-04-17 17:03:35 +0800

    fix: 添加异步通知失败重试代码

---
+++ WebApplication1/WebApplication1/Controllers/PayNotifyController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using NPOI.SS.UserModel;
+using PPPayReportTools.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using WebApplication1.Enum;
+using WebApplication1.Helper;
+using WebApplication1.Model;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/PayNotify")]
+    [ApiController]
+    public class PayNotifyController : ControllerBase
+    {
+        protected HttpClient PayHttpClient { get; set; }
+        public ExcelHelper ExcelHelper { get; set; }
+        public IWebHostEnvironment WebHostEnvironment { get; set; }
+        public ILogger Logger { get; set; }
+        public IConfiguration Configuration { get; set; }
+        public ESSearchHelper ESSearchHelper { get; set; }
+
+        public PayNotifyController(
+            IHttpClientFactory httpClientFactory,
+            ExcelHelper excelHelper,
+            IWebHostEnvironment webHostEnvironment,
+            ILogger<TestController> logger,
+            IConfiguration configuration,
+            ESSearchHelper eSSearchHelper)
+        {
+            this.PayHttpClient = httpClientFactory.CreateClient();
+            this.ExcelHelper = excelHelper;
+            this.WebHostEnvironment = webHostEnvironment;
+            this.Logger = logger;
+            this.Configuration = configuration;
+            this.ESSearchHelper = eSSearchHelper;
+        }
+
+        /// <summary>
+        /// ES搜索订单支付方式
+        /// </summary>
+        /// <returns></returns>
+        [Route("")]
+        [HttpGet]
+        public async Task<IActionResult> ESSearchOrderPayType()
+        {
+            string dataFilter = @"[
+    {
+        ""multi_match"": {
+            ""type"": ""phrase"",
+            ""query"": ""MeShopPay无法同步支付结果到网站"",
+            ""lenient"": true
+        }
+    }
+]";
+
+            List<ESLog> esLogList = await this.ESSearchHelper.GetESLogList($"获取Pay通知失败数据", dataFilter, 1, log =>
+            {
+                return "1";
+            });
+
+            int totalCount = esLogList.Count;
+            this.Logger.LogInformation($"获取到同步失败数据共{totalCount}个");
+            string sessionID, notifyUrl;
+            Regex sessionIDRegex = new Regex("(?<=\"token\":\")[a-z0-9]+(?=\")");
+            Regex notifyUrlRegex = new Regex("(?<=\"NotifyUrl\":\")[^\"]+(?=\")");
+            int position = 1;
+            bool isSend = false;
+            foreach (ESLog log in esLogList)
+            {
+                sessionID = sessionIDRegex.Match(log.Log).Value;
+                notifyUrl = notifyUrlRegex.Match(log.Log).Value + "?sessionID=" + sessionID;
+                isSend = false;
+                do
+                {
+                    var postResult = await this.PayHttpClient.Post(notifyUrl, null);
+                    isSend = postResult.Item1 == System.Net.HttpStatusCode.OK;
+                    this.Logger.LogInformation($"正在同步{position}/{totalCount}个异步通知消息到网站:{postResult.Item1},detail:{{notifyUrl={notifyUrl}}}");
+                } while (isSend == false);
+
+                position++;
+            }
+
+            this.Logger.LogInformation($"任务结束.");
+
+            return Ok();
+        }
+    }
+}
--- WebApplication1/WebApplication1/Helper/ESSearchHelper.cs
+++ WebApplication1/WebApplication1/Helper/ESSearchHelper.cs
@@ -20,162 +20,49 @@ namespace WebApplication1.Helper
 
         public async Task<List<ESLog>> GetESLogList(string logFlag, string dataFilter, int beginDays, Func<string, string> logTypeFunc)
         {
-            //想要快速查询：需要打开ES搜索页，放置一个定时器定时输入耗时较长的搜索词点击查询按钮，程序方可快速查询数据
-            //window.setInterval(function(){document.querySelector("button.euiSuperUpdateButton").click();},2000)
-            //搜索内容：挑选耗时比较长的搜索词，让ES处于搜索中状态，如："a" and ("/ajax/paydd" or "/ajax/pay")  
-            //搜索时间：可选定2个月/年等较长时间段，让ES处于搜索中状态
-            //下面查询语句注意搜索时间段
-
-
             List<ESLog> logList = new List<ESLog>(100);
 
-            #region 获取requestID
+            //ES搜索文档：https://www.elastic.co/guide/en/elasticsearch/reference/8.1/query-dsl.html
+            //ES搜索测试控制台：https://log.meshopstore.com/app/dev_tools#/console
+            //ES参考方案可以从：https://log.meshopstore.com/app/discover 获取搜索结构后，再将请求日志中的请求参数中的filter下的参数拿来测试
 
             var body = @"
 {
-    ""params"": {
-        ""ignoreThrottled"": true,
-        ""index"": ""logstash-*"",
-        ""body"": {
-            ""version"": true,
-            ""size"": 500,
-            ""sort"": [
+    ""size"": 500,
+    ""query"": {
+        ""bool"": {
+            ""must"": [],
+            ""filter"": [
                 {
-                    ""@timestamp"": {
-                        ""order"": ""desc"",
-                        ""unmapped_type"": ""boolean""
+                    ""bool"": {
+                        ""filter"": " + dataFilter + @"
                     }
-                }
-            ],
-            ""aggs"": {
-                ""2"": {
-                    ""date_histogram"": {
-                        ""field"": ""@timestamp"",
-                        ""fixed_interval"": ""1h"",
-                        ""time_zone"": ""Asia/Shanghai"",
-                        ""min_doc_count"": 1
-                    }
-                }
-            },
-            ""stored_fields"": [
-                ""*""
-            ],
-            ""script_fields"": {},
-            ""docvalue_fields"": [
-                {
-                    ""field"": ""@timestamp"",
-                    ""format"": ""date_time""
                 },
                 {
-                    ""field"": ""t.$date"",
-                    ""format"": ""date_time""
-                },
-                {
-                    ""field"": ""timestamp"",
-                    ""format"": ""date_time""
-                }
-            ],
-            ""_source"": {
-                ""excludes"": []
-            },
-            ""query"": {
-                ""bool"": {
-                    ""must"": [],
-                    ""filter"": [
-                        {
-                            ""bool"": {
-                                ""filter"": " + dataFilter + @"
-                            }
-                        },
-                        {
-                            ""range"": {
-                                ""@timestamp"": {
-                                    ""gte"": """ + DateTime.UtcNow.AddDays(-1 * beginDays).ToString("yyyy-MM-ddTHH:mm:ss") + @""",
-                                    ""lte"": """ + DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss") + @""",
-                                    ""format"": ""strict_date_optional_time""
-                                }
-                            }
+                    ""range"": {
+                        ""@timestamp"": {
+                            ""gte"": """ + DateTime.UtcNow.AddDays(-1 * beginDays).ToString("yyyy-MM-ddTHH:mm:ss") + @""",
+                            ""lte"": """ + DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss") + @""",
+                            ""format"": ""strict_date_optional_time""
                         }
-                    ],
-                    ""should"": [],
-                    ""must_not"": []
+                    }
                 }
-            },
-            ""highlight"": {
-                ""pre_tags"": [
-                    ""@kibana-highlighted-field@""
-                ],
-                ""post_tags"": [
-                    ""@/kibana-highlighted-field@""
-                ],
-                ""fields"": {
-                    ""*"": {}
-                },
-                ""fragment_size"": 2147483647
-            }
-        },
-        ""rest_total_hits_as_int"": true,
-        ""ignore_unavailable"": true,
-        ""ignore_throttled"": true,
-        ""preference"": 1649829310063,
-        ""timeout"": ""30000ms""
+            ],
+            ""should"": [],
+            ""must_not"": []
+        }
     }
 }";
-            #endregion
 
-            string requestID = null;
+            this.Logger.LogInformation($"{logFlag}正在获取查询结果...");
 
-            int trySearchIDCount = 1;
-            do
+            string postUrl = "https://log.meshopstore.com/api/console/proxy?path=logstash-%2A%2F_search&method=GET";
+            Dictionary<string, string> postHeaderDic = new Dictionary<string, string>
             {
-                try
-                {
-                    var responseResult1 = await this.PayHttpClient.Post("https://log.meshopstore.com/internal/search/es", body, headerDict: new Dictionary<string, string>
-                {
-                    {"kbn-version", "7.9.3" }
-                });
-
-                    requestID = JObject.Parse(responseResult1.Item2).SelectToken("id")?.ToObject<string>();
-
-                    this.Logger.LogInformation($"{logFlag}正在获取查询ID,尝试第{trySearchIDCount}次查询：{requestID}");
-                    trySearchIDCount++;
-                }
-                catch (Exception)
-                {
-                }
-
-            } while (string.IsNullOrEmpty(requestID));
-
-
-            int trySearchResultCount = 1;
-            body = @"{
-                        ""id"":""" + requestID + @"""
-                    }";
-            string responseResult2 = null;
-            bool isRuning = false;
-            do
-            {
-                try
-                {
-                    isRuning = true;
-                    responseResult2 = (await this.PayHttpClient.Post("https://log.meshopstore.com/internal/search/es", body, headerDict: new Dictionary<string, string>
-                    {
-                        {"kbn-version", "7.9.3" }
-                    })).Item2;
-                    isRuning = JObject.Parse(responseResult2).SelectToken("is_running")?.ToObject<bool>() ?? false;
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
-                if (isRuning)
-                {
-                    this.Logger.LogInformation($"{logFlag}正在读取查询结果,ES正在运行中,尝试第{trySearchResultCount}次查询：{requestID}");
-                }
-                trySearchResultCount++;
-            } while (string.IsNullOrEmpty(responseResult2) || isRuning);
-
-            JArray hitJArray = JObject.Parse(responseResult2).SelectToken("rawResponse.hits.hits")?.ToObject<JArray>();
+                {"kbn-xsrf", "true" }
+            };
+            string responseResult2 = (await this.PayHttpClient.Post(postUrl, body, headerDict: postHeaderDic)).Item2;
+            JArray hitJArray = JObject.Parse(responseResult2).SelectToken("hits.hits")?.ToObject<JArray>();
             foreach (JObject item in hitJArray)
             {
                 string log = item.SelectToken("_source.log").ToObject<string>();
