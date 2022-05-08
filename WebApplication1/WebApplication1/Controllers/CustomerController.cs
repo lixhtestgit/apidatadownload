dd6b73c249ecd837fbc8f807df68d81a9ef883ff
lixianghong <lixianghong@meshop.net>
2022-05-08 13:48:40 +0800

    会计师数据收集

---
+++ WebApplication1/WebApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,213 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using NPOI.SS.UserModel;
+using PPPayReportTools.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using WebApplication1.Model;
+
+namespace WebApplication1.Controllers
+{
+    /// <summary>
+    /// 会计师数据收集控制器
+    /// </summary>
+    [Route("api/Customer")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        protected HttpClient PayHttpClient { get; set; }
+        public ExcelHelper ExcelHelper { get; set; }
+        public ILogger Logger { get; set; }
+
+        public static string ExistExcelFilePath = @"C:\Users\lixianghong\Desktop\20220508053232.xlsx";
+        public static string CustomerDetailUrl = "http://newims.lekaowang.cn/Site/Sale/ModifyPublicList.aspx?CustomerID={CustomerID}&RealName={RealName}&Tel={Tel}";
+        public static string CustomerListUrl = "http://newims.lekaowang.cn/InterfaceLibrary/Sale/Handler_List.ashx?FunName=GetPublicList&Conditions=AllCondition&SearchField=Tel&Method=%E6%8E%A8%E5%B9%BF%E9%A1%B5&ExtendSubject=%E4%B8%AD%E7%BA%A7%E4%BC%9A%E8%AE%A1&CustomerType=%E6%99%AE%E9%80%9A%E6%95%B0%E6%8D%AE";
+        public static int PageSize = 1000;
+        public static int TotalCount = 0;
+        public static Dictionary<string, string> HeadDic = new Dictionary<string, string>
+        {
+            { "Cookie","LKNewCRMUserEmail=wenjie.cui1@sinodq.net;aliyungf_tc=fe985142363e8f52f1167d51d36c95829d55bc0ed1e2c724ff7a59e8c2d4ef2c;CheckCode=96964;ASP.NET_SessionId=qpy0vvhzr5t4kjlxsame4qgs"}
+        };
+        public static List<Customer> DataList { get; set; }
+        public static List<Customer> SourceDataList { get; set; }
+
+        public CustomerController(
+            IHttpClientFactory httpClientFactory,
+            ExcelHelper excelHelper,
+            ILogger<TestController> logger)
+        {
+            this.PayHttpClient = httpClientFactory.CreateClient();
+            this.ExcelHelper = excelHelper;
+            this.Logger = logger;
+        }
+
+        [Route("")]
+        [HttpGet]
+        public IActionResult ESSearchOrderPayType()
+        {
+            CustomerController.DataList = ExcelHelper.ReadTitleDataList<Customer>(CustomerController.ExistExcelFilePath, new ExcelFileDescription());
+            CustomerController.SourceDataList = new List<Customer>(300000);
+            IWorkbook workbook = null;
+
+            var getPageResult = this.PayHttpClient.Post(CustomerController.CustomerListUrl, new Dictionary<string, string>
+            {
+                {"_search","false" },
+                {"nd","1651849194124" },
+                {"page", "1"},
+                {"rows",CustomerController.PageSize.ToString() },
+                {"sidx","RepeatDate" },
+                {"sord","desc" }
+            }, CustomerController.HeadDic).Result;
+
+            CustomerController.TotalCount = JObject.Parse(getPageResult.Item2).SelectToken("records").ToObject<int>();
+            int maxPageCount = JObject.Parse(getPageResult.Item2).SelectToken("total").ToObject<int>();
+
+            HashSet<string> customerIDHashSet = new HashSet<string>(CustomerController.TotalCount);
+            List<Customer> removeCFList = new List<Customer>(CustomerController.TotalCount);
+            foreach (var item in CustomerController.DataList)
+            {
+                if (customerIDHashSet.Contains(item.CustomerID) == false)
+                {
+                    customerIDHashSet.Add(item.CustomerID);
+                    removeCFList.Add(item);
+                }
+            }
+            CustomerController.DataList = removeCFList;
+
+            #region 多线程
+
+            //int theadCount = 50;
+            //int oneTheadPageCount = maxPageCount / theadCount;
+            //List<Task> allTask = new List<Task>(theadCount);
+
+            //for (int i = 1; i <= theadCount; i++)
+            //{
+            //    int beginPage = (i - 1) * oneTheadPageCount + 1;
+            //    int endPage = i * oneTheadPageCount;
+            //    if (i == theadCount)
+            //    {
+            //        endPage = maxPageCount;
+            //    }
+            //    allTask.Add(this.GetDataList($"线程{i}", beginPage, endPage, Order1Controller.PageSize));
+            //}
+
+            //bool allTaskIsComplate = true;
+            //do
+            //{
+            //    allTaskIsComplate = true;
+            //    foreach (Task item in allTask)
+            //    {
+            //        if (item.IsCompleted == false)
+            //        {
+            //            allTaskIsComplate = false;
+            //        }
+            //    }
+            //} while (allTaskIsComplate == false);
+
+            #endregion
+
+            #region 单线程
+
+            this.GetDataList("线程1", 1, maxPageCount, CustomerController.PageSize).Wait();
+
+            #endregion
+
+            string filePath = $@"C:\Users\lixianghong\Desktop\{DateTime.Now.ToString("yyyyMMddHHmmss")}.xlsx";
+
+            CustomerController.DataList = CustomerController.DataList.OrderByDescending(m => m.RepeatDate).ToList();
+
+            workbook = ExcelHelper.CreateOrUpdateWorkbook(CustomerController.DataList);
+            ExcelHelper.SaveWorkbookToFile(workbook, filePath);
+
+            workbook = ExcelHelper.CreateOrUpdateWorkbook(CustomerController.SourceDataList);
+            ExcelHelper.SaveWorkbookToFile(workbook, $@"C:\Users\lixianghong\Desktop\未去重_{DateTime.Now.ToString("yyyyMMddHHmmss")}.xlsx");
+
+            this.Logger.LogInformation($"任务结束.");
+
+            return Ok();
+        }
+
+        [Route("UpdateCustomerPhone")]
+        [HttpGet]
+        public async Task UpdateCustomerPhone()
+        {
+            List<Customer> dataList = ExcelHelper.ReadTitleDataList<Customer>(CustomerController.ExistExcelFilePath, new ExcelFileDescription());
+            List<Customer> emptyPhoneList = dataList.FindAll(m => string.IsNullOrEmpty(m.Phone));
+
+            Regex phoneRegex = new Regex("(?<=\")\\d{9,13}(?=\")");
+            string customerDetailUrl = CustomerController.CustomerDetailUrl;
+            foreach (Customer cus in emptyPhoneList)
+            {
+                customerDetailUrl = customerDetailUrl.Replace("{CustomerID}", cus.CustomerID).Replace("{RealName}", cus.RealName).Replace("{Tel}", cus.Tel);
+                var getResult = await this.PayHttpClient.Get(customerDetailUrl);
+                cus.Phone = phoneRegex.Match(getResult.Item2).Value;
+            }
+            IWorkbook workbook = ExcelHelper.CreateOrUpdateWorkbook(dataList);
+            ExcelHelper.SaveWorkbookToFile(workbook, $@"C:\Users\lixianghong\Desktop\{DateTime.Now.ToString("yyyyMMddHHmmss")}.xlsx");
+
+            this.Logger.LogInformation($"任务结束.");
+        }
+
+        public async Task GetDataList(string name, int minPage, int maxPage, int pageSize)
+        {
+            int page = minPage;
+
+            Regex phoneRegex = new Regex("(?<=\")\\d{9,13}(?=\")");
+
+            do
+            {
+                this.Logger.LogInformation($"{name}正在查询{page}/{maxPage}页数据...");
+                var rr = await this.PayHttpClient.Post(CustomerController.CustomerListUrl, new Dictionary<string, string>
+                {
+                    {"_search","false" },
+                    {"nd","1651849194124" },
+                    {"rows",pageSize.ToString() },
+                    {"page",page.ToString() },
+                    {"sidx","RepeatDate" },
+                    {"sord","desc" }
+                }, new Dictionary<string, string>
+                {
+                    { "Cookie","LKNewCRMUserEmail=wenjie.cui1@sinodq.net;aliyungf_tc=fe985142363e8f52f1167d51d36c95829d55bc0ed1e2c724ff7a59e8c2d4ef2c;CheckCode=96964;ASP.NET_SessionId=qpy0vvhzr5t4kjlxsame4qgs"}
+                });
+
+                List<Customer> dataJArray = JObject.Parse(rr.Item2).SelectToken("rows").ToObject<List<Customer>>();
+                int cusIndex = 1;
+                Customer data = null;
+                foreach (Customer cus in dataJArray)
+                {
+                    this.Logger.LogInformation($"{name}正在查询{page}/{maxPage}页第{cusIndex + (page - 1) * pageSize}/{CustomerController.TotalCount}个_手机号数据...");
+                    data = CustomerController.DataList.FirstOrDefault(m => m.CustomerID == cus.CustomerID);
+                    if (data != null)
+                    {
+                        cus.Phone = data.Phone;
+                    }
+
+                    if (string.IsNullOrEmpty(cus.Phone))
+                    {
+                        string customerDetailUrl = CustomerController.CustomerDetailUrl.Replace("{CustomerID}", cus.CustomerID).Replace("{RealName}", cus.RealName).Replace("{Tel}", cus.Tel);
+                        var getResult = await this.PayHttpClient.Get(customerDetailUrl);
+                        cus.Phone = phoneRegex.Match(getResult.Item2).Value;
+                    }
+
+                    if (data == null)
+                    {
+                        //去重列表添加
+                        CustomerController.DataList.Add(cus);
+                    }
+                    //原始列表添加
+                    CustomerController.SourceDataList.Add(cus);
+
+                    cusIndex++;
+                }
+                page++;
+            } while (page <= maxPage);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Model/Customer.cs
@@ -0,0 +1,84 @@
+﻿using PPPayReportTools.Excel;
+
+namespace WebApplication1.Model
+{
+    public class Customer
+    {
+        [ExcelTitle("推广科目")]
+        public string ExtendSubject { get; set; }
+
+        [ExcelTitle("推广来源")]
+        public string Method { get; set; }
+
+        [ExcelTitle("意向科目")]
+        public string SubjectName { get; set; }
+
+        [ExcelTitle("客户ID")]
+        public string CustomerID { get; set; }
+
+        [ExcelTitle("学员姓名")]
+        public string RealName { get; set; }
+
+        [ExcelTitle("客户电话ID")]
+        public string Tel { get; set; }
+
+        [ExcelTitle("手机号")]
+        public string Phone { get; set; }
+
+        [ExcelTitle("客户状态")]
+        public string Result { get; set; }
+
+        [ExcelTitle("跟进状态")]
+        public string ConnectionStatusStr
+        {
+            get
+            {
+                if (this.ConnectionStatus == 1)
+                {
+                    return "未跟进";
+                }
+                return "已跟进";
+            }
+            set
+            {
+                this.ConnectionStatus = 0;
+                if (value == "未跟进")
+                {
+                    this.ConnectionStatus = 1;
+                }
+            }
+        }
+
+        public int ConnectionStatus { get; set; }
+
+        private string _customerType { get; set; }
+        [ExcelTitle("客户类型")]
+        public string CustomerType
+        {
+            get
+            {
+                return this._customerType;
+            }
+            set
+            {
+                this._customerType = "普通数据";
+            }
+        }
+
+        [ExcelTitle("创建时间")]
+        public string CreatedDate { get; set; }
+
+        [ExcelTitle("重复时间")]
+        public string RepeatDate { get; set; }
+
+        [ExcelTitle("最近登录时间")]
+        public string LastLoginTime { get; set; }
+
+        [ExcelTitle("最近一次跟进时间")]
+        public string LastNextStepDate { get; set; }
+
+        [ExcelTitle("跟进次数")]
+        public string ActivityCount { get; set; }
+
+    }
+}
