25973ba083f94a47b73470776ceb1aa335b6cb88
lixianghong <lixianghong@meshop.net>
2022-08-29 19:32:11 +0800

    fix: 整理导出代码

--- WebApplication1/WebApplication1/Controllers/CmsDataExportController.cs
+++ WebApplication1/WebApplication1/Controllers/CmsDataExportController.cs
@@ -3,12 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using NPOI.SS.Formula.Functions;
 using NPOI.SS.UserModel;
 using PPPayReportTools.Excel;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Policy;
 using System.Threading.Tasks;
 using WebApplication1.DB.Base;
 using WebApplication1.DB.CMS;
@@ -134,8 +136,10 @@ namespace WebApplication1.Controllers
         [HttpGet]
         public void CopyDataToSite()
         {
-            //需求1：排除指定站点（0,1,11,1363,18,19,195,27,34,35,36,37,41,43,6689,6874,6876,6880,6881,6916,7162,7163,7003,7143）平均分配到对应10个站点中；
-            //需求2：将www.beddinginn.com,www.oroyalcars.com(19,1363)将原始订单数据直接搬迁到独立表
+            string beginDate = "2022-07-01";
+            string endDate = "2022-08-01";
+
+            #region 需求1：排除指定站点（0,1,11,1363,18,19,195,27,34,35,36,37,41,43,6689,6874,6876,6880,6881,6916,7162,7163,7003,7143）平均分配到对应10个站点中
 
             string tjOrderSql = @$"SELECT u.Email UserEmail,
                             (SELECT SUM(ob.BuyCount) FROM dbo.TB_OrderBill ob WHERE ob.SiteID=o.SiteID AND ob.OrderID=o.ID) ProductCount,
@@ -144,7 +148,7 @@ namespace WebApplication1.Controllers
                             LEFT JOIN dbo.TB_Site s ON s.ID=o.SiteID
                             LEFT JOIN dbo.TB_Users AS u ON o.UserID = U.ID AND o.SiteID = u.SiteID
                             LEFT JOIN dbo.TB_UserSendAddressOrder AS a WITH (NOLOCK) ON o.SiteID = a.SiteID AND a.OrderID = o.ID AND a.AddressID = o.Address1
-                            WHERE o.AddTime>'2022-07-01' AND o.AddTime<'2022-08-01' AND o.SiteID NOT IN (0,1,11,1363,18,19,195,27,34,35,36,37,41,43,6689,6874,6876,6880,6881,6916,7162,7163,7003,7143)
+                            WHERE o.AddTime>'{beginDate}' AND o.AddTime<'{endDate}' AND o.SiteID NOT IN (0,1,11,1363,18,19,195,27,34,35,36,37,41,43,6689,6874,6876,6880,6881,6916,7162,7163,7003,7143)
                             Order By o.AddTime";
             List<TJ_TB_Order> awaitOrderJObjList = this._baseRepository.QueryAsync<TJ_TB_Order>(EDBConnectionType.SqlServer, tjOrderSql).Result;
 
@@ -174,7 +178,7 @@ namespace WebApplication1.Controllers
                 try
                 {
                     orderObjPosition++;
-                    this._logger.LogInformation($"正在同步1月第{orderObjPosition}/{orderObjTotalCount}个订单...");
+                    this._logger.LogInformation($"正在同步需求1第{orderObjPosition}/{orderObjTotalCount}个订单...");
 
                     int fpSiteID = 0;
                     int randomI = random.Next(1, 101);
@@ -214,13 +218,46 @@ namespace WebApplication1.Controllers
                 }
             };
 
+            int waitObjIndex = 0;
+            List<Task> syncTaskList = new List<Task>(10);
+            bool allIsSync = true;
+            do
+            {
+                allIsSync = true;
+                if (syncTaskList.Count < 10 && waitObjIndex <= awaitOrderJObjList.Count - 1)
+                {
+                    Task syncTask = syncOrderToNewSiteFunc(awaitOrderJObjList[waitObjIndex]);
+                    syncTaskList.Add(syncTask);
+                    waitObjIndex++;
+                }
+                //检测异步方法是否全部执行完毕
+                allIsSync = syncTaskList.Exists(m => m.IsCompleted == false) ? false : true;
+                //移除已完成任务，安排下一对象同步
+                syncTaskList.RemoveAll(x => x.IsCompleted == true);
+            } while (allIsSync == false || waitObjIndex < awaitOrderJObjList.Count);
+
+            #endregion
+
+            #region 需求2：将www.beddinginn.com,www.oroyalcars.com(19,1363)将原始订单数据直接搬迁到独立表
+
+            tjOrderSql = @$"SELECT u.Email UserEmail,
+                            (SELECT SUM(ob.BuyCount) FROM dbo.TB_OrderBill ob WHERE ob.SiteID=o.SiteID AND ob.OrderID=o.ID) ProductCount,
+                            a.CountryID,
+                            o.* FROM dbo.TB_Order o
+                            LEFT JOIN dbo.TB_Site s ON s.ID=o.SiteID
+                            LEFT JOIN dbo.TB_Users AS u ON o.UserID = U.ID AND o.SiteID = u.SiteID
+                            LEFT JOIN dbo.TB_UserSendAddressOrder AS a WITH (NOLOCK) ON o.SiteID = a.SiteID AND a.OrderID = o.ID AND a.AddressID = o.Address1
+                            WHERE o.AddTime>'{beginDate}' AND o.AddTime<'{endDate}' AND o.SiteID IN (19,1363)
+                            Order By o.AddTime";
+            awaitOrderJObjList = this._baseRepository.QueryAsync<TJ_TB_Order>(EDBConnectionType.SqlServer, tjOrderSql).Result;
+
             //同步原始订单到原站点
             Func<TJ_TB_Order, Task> syncOrderFunc = async (orderObj) =>
             {
                 try
                 {
                     orderObjPosition++;
-                    this._logger.LogInformation($"正在同步第{orderObjPosition}/{orderObjTotalCount}个订单...");
+                    this._logger.LogInformation($"正在同步需求2第{orderObjPosition}/{orderObjTotalCount}个订单...");
 
                     //添加订单
                     orderObj.OriginID = orderObj.ID;
@@ -244,15 +281,15 @@ namespace WebApplication1.Controllers
                 }
             };
 
-            int waitObjIndex = 0;
-            List<Task> syncTaskList = new List<Task>(10);
-            bool allIsSync = true;
+            waitObjIndex = 0;
+            syncTaskList = new List<Task>(10);
+            allIsSync = true;
             do
             {
                 allIsSync = true;
                 if (syncTaskList.Count < 10 && waitObjIndex <= awaitOrderJObjList.Count - 1)
                 {
-                    Task syncTask = syncOrderToNewSiteFunc(awaitOrderJObjList[waitObjIndex]);
+                    Task syncTask = syncOrderFunc(awaitOrderJObjList[waitObjIndex]);
                     syncTaskList.Add(syncTask);
                     waitObjIndex++;
                 }
@@ -261,6 +298,37 @@ namespace WebApplication1.Controllers
                 //移除已完成任务，安排下一对象同步
                 syncTaskList.RemoveAll(x => x.IsCompleted == true);
             } while (allIsSync == false || waitObjIndex < awaitOrderJObjList.Count);
+
+            #endregion
+
+            #region 需求3
+
+            //执行SQL查询数据整理到Excel中：
+            var huizongSQL = @"
+            SELECT ID, Name,
+            (SELECT SUM(Price_PreCount1) FROM dbo.TJ_TB_Order WHERE SiteID = TB_Site.ID AND AddTime >= '2022-01-01' AND AddTime < '2022-02-01')[1月美金汇总],
+            (SELECT SUM(Price_PreCount1) FROM dbo.TJ_TB_Order WHERE SiteID = TB_Site.ID AND AddTime>= '2022-02-01' AND AddTime<'2022-03-01')[2月美金汇总],
+            (SELECT SUM(Price_PreCount1) FROM dbo.TJ_TB_Order WHERE SiteID = TB_Site.ID AND AddTime>= '2022-03-01' AND AddTime<'2022-04-01')[3月美金汇总],
+            (SELECT SUM(Price_PreCount1) FROM dbo.TJ_TB_Order WHERE SiteID = TB_Site.ID AND AddTime>= '2022-04-01' AND AddTime<'2022-05-01')[4月美金汇总],
+            (SELECT SUM(Price_PreCount1) FROM dbo.TJ_TB_Order WHERE SiteID = TB_Site.ID AND AddTime>= '2022-05-01' AND AddTime<'2022-06-01')[5月美金汇总],
+            (SELECT SUM(Price_PreCount1) FROM dbo.TJ_TB_Order WHERE SiteID = TB_Site.ID AND AddTime>= '2022-06-01' AND AddTime<'2022-07-01')[6月美金汇总],
+            (SELECT SUM(Price_PreCount1) FROM dbo.TJ_TB_Order WHERE SiteID = TB_Site.ID AND AddTime>= '2022-07-01' AND AddTime<'2022-08-01')[7月美金汇总]
+            FROM TB_Site where ID IN(6546, 6691, 6738, 6903, 6938, 6983, 7027, 7203, 7204, 7207, 7211, 7224)
+            ";
+
+            var xiangqingSQL= @"
+            select o.SiteID[站点ID],
+                (SELECT s.Name FROM dbo.TB_Site s WHERE s.ID=o.SiteID)[站点名称],
+                ID[订单ID],AddTime[创建时间],o.CurrencyName[币种名称],
+                CASE WHEN o.CurrencyPrice IS NULL OR o.CurrencyPrice<=0 THEN o.Price_PreCount1 ELSE o.CurrencyPrice END[多币种金额],
+                o.Price_PreCount1[美金金额]
+            from dbo.TJ_TB_Order o 
+            WHERE AddTime>='2022-07-01' and AddTime<'2022-08-01' 
+            AND o.SiteID IN (6546,6691,6738,6903,6938,6983,7027,7203,7204,7207,7211,7224)
+            ORDER by AddTime
+            ";
+
+            #endregion
 
             this._logger.LogInformation("订单处理完成！");
         }
